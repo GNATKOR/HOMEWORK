0f05b6f71b59030a6ad32f90959408f32baee06b
GNATKOR <gnatkor@gmail.com>
2024-01-22 20:02:43 +0300

    HW done and signin bug was fixed

--- lesson_24/DjangoHOMEWORK/authentication/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Users(models.Model):
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.email
